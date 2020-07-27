daee5b6d5114f3d55bc159c20e50082d2573a5ee
cr7as7 <47944258+cr7as7@users.noreply.github.com>
2020-07-27 16:07:22 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+ const express = require("express");
+ const bodyParser = require("body-parser");
+ const request = require("request");
+const https = require("https");
+
+ const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded(
+ { extended: true }
+));
+
+
+ app.get("/", function(req,res) {
+ res.sendFile(__dirname + "/signup.html")
+
+});
+
+app.post("/", function (req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
+const jsonData = JSON.stringify(data);
+
+
+const url = "https://us10.api.mailchimp.com/3.0/lists/54babff42f"
+
+const options = {
+method: "POST",
+auth: "amber:bb129242aa2745ac1d5a0fc8e9737014-us10"
+
+}
+const request = https.request(url, options, function (response) {
+
+if (response.statusCode == 200)
+{
+res.sendFile(__dirname + "/success.html");
+}
+else{
+res.sendFile(__dirname + "/failure.html");
+}
+response.on("data", function (data){
+    console.log(JSON.parse(data));
+})
+
+   })
+
+
+request.write(jsonData);
+request.end();
+
+
+});
+
+app.post("/failure", function(res, res){
+res.redirect("/")
+})
+
+ app.listen(process.env.PORT || 3000, function () {
+console.log("Server is running on port 3000");
+
+});
+
+// API KEY bb129242aa2745ac1d5a0fc8e9737014-us10
+
+//List id 54babff42f
